2ad9b9e3bec0bdadce5b7d45b1e0787b13ce14d8
Candice Peters <candice.peters@flatironschool.com>
2021-06-24 18:12:18 -0500

    lecture notes

---
+++ app/models/shoe.rb
@@ -0,0 +1,13 @@
+class Shoe < ApplicationRecord
+    def name_and_color 
+        "#{self.name} - #{self.color}"
+    end
+
+    def self.order_by_price 
+        self.order(price: :desc).limit(1)
+    end
+
+    # def self.most_expensive 
+    #     self.where("LIMIT 1")
+    # end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get '/shoes/most_expensive', to: 'shoes#most_expensive'
+  get '/shoes', to: 'shoes#index'
+  get '/shoes/:id', to: 'shoes#show', as: 'shoe'
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
