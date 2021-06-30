50081fb6eb1f9cc882110d77d7920bb2f081ef19
Candice Peters <candice.peters@flatironschool.com>
2021-06-30 14:45:13 -0400

    lesson complete

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -7,8 +7,49 @@ class ShoesController < ApplicationController
         @shoe = Shoe.find_by_id(params[:id])
     end
 
+    def new 
+        @shoe = Shoe.new 
+    end
+
+    def create 
+        @shoe = Shoe.new(shoe_params)
+        if @shoe.save
+            redirect_to shoes_path
+        else
+            render :new
+        end
+    end
+
+    def edit 
+        @shoe = Shoe.find_by_id(params[:id])
+    end
+
+    def update 
+        @shoe = Shoe.find_by_id(params[:id])
+        @shoe.update(shoe_params)
+        if @shoe.valid?
+            redirect_to shoe_path(@shoe)
+        else
+            render :edit
+        end
+    end
+
     def most_expensive
         @shoes = Shoe.order_by_price
         render :index
+    end
+
+    def destroy
+        @shoe = Shoe.find_by_id(params[:id])
+        @shoe.destroy 
+        redirect_to shoes_path
+    end
+
+    
+
+    private 
+
+    def shoe_params
+        params.require(:shoe).permit(:name, :color, :price, :condition)
     end
 end
